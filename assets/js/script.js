d7c14f3674c0e9fabb65485217f5972d642e8776
natejmaster@gmail.com <natejmaster@gmail.com>
2023-07-10 18:34:46 -0500

    Created function for incrementing questions. Now display more than one question

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,24 @@ let timeRemaining = document.querySelector("#time-remaining");
 let questionEl = document.querySelector("#question");
 let answersList = document.querySelector("#answers");
 let correctAnswer = "";
+//Creates an array of possible questions
+let questions = [
+    "Question 1",
+    "Question 2",
+    "Question 3",
+    "Question 4",
+    "Question 5"
+  ];
+//Creates an array of possible answers for each question (these are linked to the corresponding questions in the array above-- i.e. they have the same index)
+let answers = [
+    ["Q1A1", "Q1A2", "Q1A3", "Q1A4"],
+    ["Q2A1", "Q2A2", "Q2A3", "Q2A4"],
+    ["Q3A1", "Q3A2", "Q3A3", "Q3A4"],
+    ["Q4A1", "Q4A2", "Q4A3", "Q4A4"],
+    ["Q5A1", "Q5A2", "Q5A3", "Q5A4"],
+  ];
 
+let currentQuestionIndex = 0;
 //Countdown function that runs the 60 second timer for the game
 function countdown() {
   let timer = 60;
@@ -19,24 +36,22 @@ function countdown() {
 }
 
 //Function that displays the question and a randomized set of answers
-function displayQuestion(question, answers) {
+  function displayQuestion() {
+    let question = questions[currentQuestionIndex];
+    let answerOptions = answers[currentQuestionIndex];
   questionEl.textContent = question;
-
-  //Clear previous answers
   answersList.innerHTML = "";
 
-  //Set the correctAnswer variable as the first answer in the answers array.
-  correctAnswer = answers[0];
-  // Shuffle the answers array
-  let shuffledAnswers = shuffleArray(answers);
+  let shuffledAnswers = shuffleArray(answerOptions);
 
-  // Create buttons for each answer that will show up in a random order
   shuffledAnswers.forEach(function (answer) {
     let button = document.createElement("button");
     button.textContent = answer;
     button.classList.add("answer-choice");
     answersList.appendChild(button);
   });
+
+  correctAnswer = answerOptions[0];
 }
 
 //Shuffle the answers relatively randomly using a Fisher-Yates shuffle (researched on Stack Overflow: https://stackoverflow.com/questions/59810241/how-to-fisher-yates-shuffle-a-javascript-array)
@@ -51,31 +66,8 @@ function shuffleArray(array) {
 //Event listener for the Start Button that runs the countdown function when the Start Button is clicked
 startButton.addEventListener('click', function () {
   countdown();
-
-  //Creates an array of possible questions
-  let questions = [
-    "Question 1",
-    "Question 2",
-    "Question 3",
-    "Question 4",
-    "Question 5"
-  ];
-  //Creates an array of possible answers for each question (these are linked to the corresponding questions in the array above-- i.e. they have the same index)
-  let answers = [
-    ["Q1A1", "Q1A2", "Q1A3", "Q1A4"],
-    ["Q2A1", "Q2A2", "Q2A3", "Q2A4"],
-    ["Q3A1", "Q3A2", "Q3A3", "Q3A4"],
-    ["Q4A1", "Q4A2", "Q4A3", "Q4A4"],
-    ["Q5A1", "Q5A2", "Q5A3", "Q5A4"],
-  ];
-  //Randomly chooses a number based on the Math.floor and Math.random elements multiplied by the length of the array.
-  let randomIndex = Math.floor(Math.random() * questions.length);
-  //The question is whichever index # was just selected
-  let question = questions[randomIndex];
-  //The answers are the array of four choices based on the index # just selected.
-  let answerOptions = answers[randomIndex];
-
-  displayQuestion(question, answerOptions);
+  currentQuestionIndex++;
+  displayQuestion();
 });
 
 //Adds an event listener for each button
@@ -93,5 +85,24 @@ answersList.addEventListener('click', function (event) {
         //This alerts the player  that they got the question wrong.
       window.alert("Incorrect!");
     }
+    //Increment the current question
+    currentQuestionIndex++;
+    // Check if there are more questions available
+    if (currentQuestionIndex < questions.length) {
+      // Display the next question
+      displayQuestion(currentQuestionIndex);
+    } else {
+      // No more questions, game over
+      window.alert("Game Over!");
+    }
   }
 });
+
+//TO DO:
+//Change correct/incorrect function to adding score if correct and subtracting time from timer if incorrect.
+//Create function to move on to next question after a choice has been made
+//End game when timer hits zero (or all questions are answered)
+//Commit final score to local storage with player's initials
+  //This also means we need an input for player's initials
+//Display current high scores on a standings board
+//If time, CSS styling?
