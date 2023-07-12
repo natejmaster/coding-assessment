d49c381adf2800df3d6795cc6158085eb33d8546
natejmaster@gmail.com <natejmaster@gmail.com>
2023-07-12 18:09:11 -0500

    Changed confusing high score header to best times, added clarifying commentary to JS page

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ let answersList = document.querySelector("#answers");
 let highScoresList = document.querySelector("#high-scores");
 let questionBox = document.querySelector("#question-box");
 let timerBox = document.querySelector("#timer-box");
+//Empty variables that will be declared later in the algorithm
 let correctAnswer = "";
 let timerInterval;
 //Creates an array of possible questions
@@ -25,7 +26,7 @@ let answers = [
   ["Array", "Function", "Object", "Variable Pair"],
   ["playFunction()", "()playFunction", "PlayFunction()", "play-function()"],
 ];
-
+//Starts with the first question so the game can increment after each answer
 let currentQuestionIndex = 0;
 //Countdown function that runs the 60 second timer for the game
 function countdown() {
@@ -42,28 +43,35 @@ function countdown() {
   }, 1000);
 }
 
-//Function that displays the question and a randomized set of answers
+//Function that displays the question and corresponding answers in a random order
 function displayQuestion() {
+  //Matches current question to current answer set
   let question = questions[currentQuestionIndex];
   let answerOptions = answers[currentQuestionIndex];
+  //Points out that the correct answer will always be the first answer from each set in the initial declaration of the array.
   correctAnswer = answerOptions[0];
+  //Renders the question and answers
   questionEl.textContent = question;
   answersList.innerHTML = "";
-
+// Declares variable shuffledAnswers that will create the four answer buttons
   let shuffledAnswers = shuffleArray(answerOptions);
-
+//Each answer gets its own corresponding button
   shuffledAnswers.forEach(function (answer) {
     let button = document.createElement("button");
     button.textContent = answer;
     button.classList.add("answer-choice");
+    //The buttons are appended to a list of answers
     answersList.appendChild(button);
   });
 }
 
 //Shuffle the answers relatively randomly using a Fisher-Yates shuffle (researched on Stack Overflow: https://stackoverflow.com/questions/59810241/how-to-fisher-yates-shuffle-a-javascript-array)
 function shuffleArray(array) {
+  //Uses a for loop to run through the possible answer options until none remain
   for (let i = array.length - 1; i > 0; i--) {
+    //Uses a combination of Math.floor and Math.Random methods to choose one of the remaining answers to be rendered
     let j = Math.floor(Math.random() * (i + 1));
+    //Replaces the order each answer is displayed in the original array display
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
@@ -71,9 +79,11 @@ function shuffleArray(array) {
 
 //Event listener for the Start Button that runs the countdown function when the Start Button is clicked
 startButton.addEventListener('click', function () {
+  //This hides the start button and the clear high scores button during the game
   startButton.style.display = 'none';
   clearScoresButton.style.display = 'none';
   highScoresList.innerHTML = '';
+  //Once the Start Game button is pressed, the timer begins counting down and the first question is displayed.
   countdown();
   displayQuestion();
   questionBox.style.display = "block";
@@ -82,6 +92,7 @@ startButton.addEventListener('click', function () {
 
 //Event listener for the Clear Scores Button that clears high scores from localStorage:
 clearScoresButton.addEventListener("click", function () {
+  //Empties local storage and removes all entries from the high score list
   localStorage.removeItem("highScores");
   highScoresList.innerHTML = "";
 });
@@ -98,7 +109,7 @@ answersList.addEventListener('click', function (event) {
       //This alerts the player that they've gotten the question correct
       window.alert("Correct!");
     } else {
-      //This alerts the player  that they got the question wrong.
+      //This alerts the player  that they got the question wrong and subtracts 5 seconds from the timer
       window.alert("Incorrect!");
       timer -= 5;
       timer = Math.max(timer, 0);
@@ -111,13 +122,16 @@ answersList.addEventListener('click', function (event) {
       // Display the next question
       displayQuestion();
     } else {
-      // No more questions, game over
+      // When there are no more questions, the game is over, the user is alerted as such, and the timer is cleared
       window.alert("Game Over!");
       clearInterval(timerInterval);
-
+      //The user is prompted by a window.prompt to enter their initials
       const initials = prompt("Enter your initials for the High Score Board");
+      //If the user enters their initials, a score is rendered based on how much time it took to complete the quiz (with penalties for incorrect answers)
       if (initials) {
+        //An object is created with two values: the initials, and the time
         const timeEntry = { initials, time: 60 -timer };
+        //The high score is moved through the JavaScript and transformed back into a string as it is pushed into the high score list
         let highScores = JSON.parse(localStorage.getItem('highScores')) || [];
         highScores.push(timeEntry);
         localStorage.setItem('highScores', JSON.stringify(highScores));
